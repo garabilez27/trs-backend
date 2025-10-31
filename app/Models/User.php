a0c7e541e9cdc3ca163a5b7573fe92cbae774628
Wendhelle Garabilez <54738532+garabilez27@users.noreply.github.com>
2025-10-31 08:20:17 +0800

    API Auth
    -register
    -auth
    -me

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    // Customized table name
+    protected $table = 'tbl_users';
+
+    // Primary key
+    protected $primaryKey = 'usr_id';
+
+    // Disable default timestamps
+    public $timestamps = true;
+
+    // Set custom names for timestamp columns
+    const CREATED_AT = 'usr_created_at';
+    const UPDATED_AT = 'usr_updated_at';
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +32,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'usr_name',
+        'usr_email',
+        'usr_password',
+        'usr_created_at',
+        'usr_updated_at'
     ];
 
     /**
@@ -30,19 +46,25 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function getAuthIdentifierNameName()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return 'usr_email';
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->usr_password;
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
